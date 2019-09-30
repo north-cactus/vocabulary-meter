4b6b6bdda6adc856be156e00255992b2b9af4f67
north-cactus <nikita.fedorov.00@bk.ru>
2019-09-30 22:24:15 +0300

    Added logging of convertToTXT.sh

--- convertToTXT.sh
+++ convertToTXT.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-#formats=".mobi .epub .txt .fb2"
-formats=".fb2"
+formats=".mobi .epub .txt .fb2"
 indir=./data 
+log="$indir/.convertToTXT.log"
 
 converter=ebook-convert 
 converter_package=calibre
 
+date > "$log"
+echo "Converting have been started." >> "$log"
+
 conv()
 {
+	echo conv
+	date >> "$3"
+	echo "Started converting of $1" >> "$3"
 	out=`echo $1 | sed -e 's/ /_/g'`
 	out=`basename $out $2`.txt
 	ebook-convert "$1" in/$out && echo file: `basename $out .txt`
+	date >> "$3"
+	echo "Finished converting of $1" >> "$3"
 }
 export -f conv 
 
@@ -22,7 +30,6 @@ else
 	for format in $formats
 	do
 		mask="$indir/*$format"
-		echo "$mask"
-		find . -wholename "$mask" -type f -exec bash -c 'conv "{}" "$format"' \;
+		find . -wholename "$mask" -type f -exec bash -c "conv \"{}\" \"$format\" \"$log\"" \;
 	done
 fi
