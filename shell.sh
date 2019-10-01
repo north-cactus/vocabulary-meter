62805be25c8716093e30748738f0b59b13133abb
north-cactus <nikita.fedorov.00@bk.ru>
2019-10-01 13:13:47 +0300

    Added logging of shell.sh

--- shell.sh
+++ shell.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-inlist=`ls in/*.txt`
-outdir=out/
+outdir=./out/
+indir=./in/
+log=./out/.vocabularty-meter.log
+
+date > "$log"
+echo "Vocabulary-meter: processing have been started." >> "$log"
+
+inlist=`find "$indir" -name "*.txt" -type f`
 
 for i in $inlist
 do
 	outname=$outdir`basename $i .txt`.words
+	date >> "$log"
+	echo "Processing of $i have been started." >> "$log"
 	./Main $i > $outname
-	basename $outname 
+	date >> "$log"
+	echo "Processing of $i have been finished." >> "$log"
 done
+
+date >> "$log"
+echo "Vocabulary-meter: processing have been finished." >> "$log"
